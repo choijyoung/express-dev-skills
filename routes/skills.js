f6a607d4e50cc24f550eb8f4c33e89ef62f8f769
choijyoung <choi.jae.y@gmail.com>
2022-02-23 22:43:00 -0500

    initial

---
+++ routes/skills.js
@@ -0,0 +1,10 @@
+import { Router } from 'express'
+import * as skillsCtrl from '../controllers/skills.js'
+const router = Router()
+
+/* GET users listing. */
+router.get('/', skillsCtrl.index)
+
+export {
+  router
+}
