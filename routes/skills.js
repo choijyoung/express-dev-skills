91dfe404957627873c7da9ab2d914f2c580c1114
choijyoung <choi.jae.y@gmail.com>
2022-02-24 17:25:39 -0500

    finish lab

--- routes/skills.js
+++ routes/skills.js
@@ -4,7 +4,14 @@ const router = Router()
 
 /* GET users listing. */
 router.get('/', skillsCtrl.index)
+router.get('/new', skillsCtrl.new)
 router.get('/:id', skillsCtrl.show)
+
+// You want to post for create
+router.post('/', skillsCtrl.create)
+
+// Use .delete to delete :: /:id to delete the targeted ID
+router.delete('/:id', skillsCtrl.delete)
 
 export {
   router
